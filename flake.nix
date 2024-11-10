eff721da691f961ba0e95c48bc10354817108aae
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-11-10 20:14:14 +0000

    Rework the flake again

--- flake.nix
+++ flake.nix
@@ -6,32 +6,36 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { ... } @ inputs:
+  outputs = { self, ... } @ inputs:
     inputs.flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import inputs.nixpkgs {
-          inherit system;
+          system = "x86_64-linux";
+        };
+        lib = pkgs.lib;
+
+        pkgs_arm64 = import inputs.nixpkgs {
+          system = "aarch64-linux";
         };
 
-        backup_world = { pkgs }:
-          pkgs.writeShellScriptBin ''minecraft_backup'' ''
-            set -euo pipefail
-            ${pkgs.coreutils}/bin/mkdir -p $BACKUP_PATH
-            while true; do
-              ${pkgs.coreutils}/bin/sleep $BACKUP_INTERVAL
-              ${pkgs.rdiff-backup}/bin/rdiff-backup --api-version 201 --new backup $WORLD_PATH $BACKUP_PATH
-              set +e
-              ${pkgs.rdiff-backup}/bin/rdiff-backup --api-version 201 --new remove increments --older-than ''${KEEP_BACKUPS}B $BACKUP_PATH
-              set -e
-            done
-          '';
+        backup_world = { myPkgs }: myPkgs.writeShellScriptBin ''minecraft_backup'' ''
+          set -euo pipefail
+          ${myPkgs.coreutils}/bin/mkdir -p $BACKUP_PATH
+          while true; do
+            ${myPkgs.coreutils}/bin/sleep $BACKUP_INTERVAL
+            ${myPkgs.rdiff-backup}/bin/rdiff-backup --api-version 201 --new backup $WORLD_PATH $BACKUP_PATH
+            set +e
+            ${myPkgs.rdiff-backup}/bin/rdiff-backup --api-version 201 --new remove increments --older-than ''${KEEP_BACKUPS}B $BACKUP_PATH
+            set -e
+          done
+        '';
 
-        container_packages = pkgs: with pkgs; [
-          (backup_world { inherit pkgs; })
-          coreutils
-          dockerTools.binSh
-          dockerTools.caCertificates
-          ps
+        container_packages = { myPkgs }: [
+          (backup_world { inherit myPkgs; })
+          myPkgs.coreutils
+          myPkgs.dockerTools.binSh
+          myPkgs.dockerTools.caCertificates
+          myPkgs.ps
         ];
 
         container_aarch64 = pkgs.pkgsCross.aarch64-multiplatform.dockerTools.buildLayeredImage {
@@ -40,7 +44,7 @@
           config.Cmd = [ "/bin/minecraft_backup" ];
           contents = pkgs.pkgsCross.aarch64-multiplatform.buildEnv {
             name = "image-root";
-            paths = container_packages { pkgs = pkgs.pkgsCross.aarch64-multiplatform; };
+            paths = container_packages { myPkgs = pkgs_arm64; };
             pathsToLink = [ "/bin" "/etc" "/var" ];
           };
           fakeRootCommands = ''
@@ -55,7 +59,7 @@
           config.Cmd = [ "/bin/minecraft_backup" ];
           contents = pkgs.buildEnv {
             name = "image-root";
-            paths = (container_packages { inherit pkgs; });
+            paths = container_packages { myPkgs = pkgs; };
             pathsToLink = [ "/bin" "/etc" "/var" ];
           };
           fakeRootCommands = ''
@@ -65,6 +69,13 @@
         };
       in
       {
+        checks =
+          let
+            packages = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self.packages;
+            devShells = lib.mapAttrs' (n: lib.nameValuePair "devShell-${n}") self.devShells;
+          in
+          packages // devShells;
+
         packages = {
           container_x86_64 = container_x86_64;
           container_aarch64 = container_aarch64;
@@ -73,6 +84,7 @@
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.just
+            pkgs.nix-fast-build
             pkgs.podman
             pkgs.rdiff-backup
           ];
